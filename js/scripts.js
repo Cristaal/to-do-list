e7a4305769592942beb37dc92838ab23b45c011f
Crystal Contreras-Grossman and Karvari Ellingson <student@epicodus.com>
2015-02-16 16:35:12 -0800

    add checkboxes?

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     var inputtedDueDate = $("input#new-dueDate").val();
     var newTask = { task: inputtedTask, dueDate: inputtedDueDate };
 
-    $("ul#taskList").append("<li><span class='task'>" + newTask.task + "</span></li>");
+    $("ul#taskList").append("<li><input type='checkbox' id='checkOff'></input><span class='task'>" + newTask.task + "</span></li>");
+    $("ul#taskList").append("<li onclick='$(this).remove();'>" + "<input type='hidden' name='show-completed-tasks'>" + "</li>")
 
     $("input#new-task").val("");
     $("input#new-dueDate").val("");
@@ -16,5 +17,21 @@ $(document).ready(function() {
       $("#show-task h2").text(newTask.task);
       $("#dueDate").text(newTask.dueDate);
     });
+
+    $("#checkOff").last().click(function() {
+      $("#show-completed-tasks").show();
+      $("#show-completed-tasks").text(newTask.task);
+    })
   });
 });
+
+// function selectTasks(select)
+// {
+//   var $ul = $(select).prev('ul');
+//
+//   if ($ul.find('input[value=' + $(select).val() + ']').length == 0)
+//     $ul.append('<li onclick="$(this).remove();">' +
+//       '<input type="hidden" name="ingredients[]" value="' +
+//       $(select).val() + '" /> ' +
+//       $(select).find('option[selected]').text() + '</li>');
+// }
